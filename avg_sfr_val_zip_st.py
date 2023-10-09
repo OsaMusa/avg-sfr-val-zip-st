8c1adfe52a38e3f8f666cf634481728ce91f483d
Osa Musa <omusawrk@gmail.com>
2023-10-09 18:12:59 -0500

    Chore Changed data source file location in code.

--- avg_sfr_val_zip_st.py
+++ avg_sfr_val_zip_st.py
@@ -23,7 +23,7 @@ if 'default_cities' not in st.session_state:
 @st.cache_data(show_spinner='Loading Avg SFR Value Data...')
 def load_data():
     # Checking Zillow File
-    zillow_raw_df = pd.read_csv("../Zillow SFR Avg Values by ZIP (Aug 2023).csv", dtype={'RegionID':'str', 'RegionName':'str'})
+    zillow_raw_df = pd.read_csv("zhvi-sfr-zip/Zillow SFR Avg Values by ZIP (Aug 2023).csv", dtype={'RegionID':'str', 'RegionName':'str'})
     zillow_raw_df = zillow_raw_df.rename(columns={'RegionName':'ZIP', 'CountyName':'County'}).drop(columns=['RegionID', 'SizeRank', 'RegionType','StateName'])
 
     # Add Lat/Long
